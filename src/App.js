4e849e38f4323eb0ad11f71fe5db9aa5258cb372
Sungyu Kwon <kwonsun7@msu.edu>
2023-01-28 17:10:04 -0500

    Basic File Input and Text input from other repo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import script from './python/main.py';
-import logo from './logo.svg';
 import './App.css';
 
 const runScript = async (code) => {
@@ -12,7 +11,21 @@ const runScript = async (code) => {
 }
 
 const App = () => {
+  const [image, setImage] = useState();
+  const [walletkeys, setWalletKeys] = useState()
   const [output, setOutput] = useState("(loading...)");
+
+  function onImageChange(e) {
+    setImage(e.target.files);
+}
+
+function onTextInput(e){
+    setWalletKeys(e.target.value)
+}
+
+function onEncodeButtonClick(){
+    alert('Encode Button Clicked')
+}
 
   useEffect(() => {
     const run = async () => {
@@ -27,7 +40,13 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <p className='text-4xl'>Encode Screen</p>
+        <input className="m-4 w-1/4" type="file" accept="image/*" onChange={onImageChange}/>
+        <input className="w-1/4 m-4" type="text" onChange={onTextInput}/>
+        <p className='mx-4'>{walletkeys}</p>
+        <div className="bg-black w-2/12 rounded-md flex justify-center mx-4">
+            <button className="text-white" onClick={onEncodeButtonClick}>Encode Image</button>
+        </div>
         <p>
           5 + 7 = {output}
         </p>
